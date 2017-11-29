7a6c5f7c0b8cfbcdfbc47827754d0ef563ece034
DDriggs00 <DDriggs00@gmail.com>
2017-11-29 08:59:53 -0800

    Started NLP Setup

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import Dict
-# import nltk
+import nltk
 
-# Give me all the games in the database that were made before 2000
+# Give me a list of all the games in the database that were made by nintendo before 2000
 quit = 0
-while True:
-    InString = raw_input("Please enter a Query or \"Exit\" to quit\n")
-    if InString.lower() == "exit":
-        break
+# while True:
+InString = input("Please enter a Query or \"Exit\" to quit\n")
+#     if InString.lower() == "exit":
+#         break
 
-    InString = Dict.DoReplacing(InString)
+tokens = nltk.word_tokenize(InString)
+tagged = nltk.pos_tag(tokens)
+entities = nltk.chunk.ne_chunk(tagged)
+print("\n")
+print(entities)
 
-    # Create MySQL Syntax
-
-    print(InString)
+# InString = Dict.DoReplacing(InString)
+# Create MySQL Syntaxtet
+# print(InString)
